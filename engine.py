f8de6000f9ad159d7d4abbd41de330ae0525ed7b
Saharat Prangpitak <saharat.prangpitak@gmail.com>
2023-11-08 09:34:18 +0000

    add: card photos

--- engine.py
+++ engine.py
@@ -1,2 +1,15 @@
+import card
+import Board
+
+NUMBER_OF_AIS = 1
 class Engine:
-    pass
+    def __init__(self):
+        self.pool = card.cardPool()
+        self.board = board.Board()
+    
+    def distribute(self):
+        list_of_players = [player.HumanPlayer()]
+        if NUMBER_OF_AIS > 0:
+            for _ in range(NUMBER_OF_AIS):
+                list_of_players.append(AIPlayer())
+        
--- player.py
+++ player.py
@@ -3,12 +3,15 @@ class Player:
         self.hand = []
         self.turn = None
         self.score = 0
-
+    def is_human(self):
+        return isinstance(self,HumanPlayer)
+    
+    def is_ai(self):
+        return isinstance(self,AIPlayer)
 
 class HumanPlayer(Player):
     def __init__(self):
         super().__init__()
-
 
 class AIPlayer(Player):
     def __init__(self):
