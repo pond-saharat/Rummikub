ca3d4460eede2ee6056add6129b810709b8b4be9
Saharat Prangpitak <saharat.prangpitak@gmail.com>
2023-11-16 14:20:59 +0000

    Update

---
+++ image_generator.py
@@ -0,0 +1,51 @@
+# This file is for generating a card picture in *.png in ./src/*.png
+import matplotlib.pyplot as plt
+import os
+import sys
+
+from config import *
+
+joker_number = 2
+card_number = 13
+
+class ImageGenerator:
+    def __init__(self,number_of_colour_cards,number_of_joker_cards):
+        self.number_of_colour_cards = number_of_colour_cards
+        self.number_of_joker_cards = number_of_joker_cards
+    
+    def run(self,regenerate):
+        if regenerate == True:
+            try:
+                os.system("rm ./src/*.png")
+                self.generate_colour_card_png()
+                self.generate_joker_card_png()
+                print("The images were generated successfully.")
+            except:
+                raise RuntimeError("Image generation failed.")
+
+    def generate_colour_card_png(self):
+        for number in range(1,self.number_of_colour_cards+1):
+            for colour in COLOURS:
+                fig = plt.figure()
+                plt.text(1.5,2.0,s=f"{number}",c=f"{colour}",fontsize=60,horizontalalignment='center',verticalalignment='center')
+                plt.xlim([0,3]) 
+                plt.ylim([0,4]) 
+                plt.axis('off')
+                plt.gcf().set_size_inches(3,4)
+                plt.savefig(f"./src/{colour.lower()}{number}.png", dpi=300)
+                plt.close()
+
+    def generate_joker_card_png(self):
+        fig = plt.figure()
+        plt.text(1.5,2.0,s=f"Joker",c="black",fontsize=60,horizontalalignment='center',verticalalignment='center')
+        plt.xlim([0,3]) 
+        plt.ylim([0,4]) 
+        plt.axis('off')
+        plt.gcf().set_size_inches(3,4)
+        plt.savefig(f"./src/joker.png", dpi=300)
+        plt.close()
+
+image_generator = ImageGenerator(card_number, joker_number)
+image_generator.run(REGENERATE_IMAGE)
+
+
---
+++ set.py
@@ -0,0 +1,76 @@
+from config import *
+
+class Set:
+    def __init__(self,cards):
+        self.cards = cards
+
+    # Create a new set of cards
+    # List[Card] -> Group() or Run() or False (If they are not either Group or Run)
+    @classmethod
+    def create(cls, cards):
+        if cls.is_group(cards):
+            return Group(cards)
+        elif cls.is_run(cards):
+            return Run(cards)
+        else:
+            return False
+    # Check if the cards are valid for being classified as Group
+    # List[Card] -> bool
+    @staticmethod
+    def is_group(cards):
+        # Need comments
+        if len(cards) < 3:
+            return False
+        # Get the colour cards from a pack of cards
+        colour_cards = Card.get_colour_cards(cards)   
+        # Need comments
+        if len(set([card.number for card in regular_cards])) != 1:
+            return False
+        else:
+            return len(set([card.colour for card in regular_cards])) == len(regular_cards)
+
+    # Check if the cards are valid for being classified as Run
+    # List[Card] -> bool
+    @staticmethod
+    def is_run(cards):
+        # Need comments
+        if len(cards) < 3:
+            return False
+        # Need comments
+        colour_cards = Card.get_colour_cards(cards)
+        joker_cards_count = len(Card.get_joker_cards(cards)) #len([card for card in cards if card.number == 30]) 
+        # Need comments
+        if len(set([card.colour for card in regular_cards])) != 1:
+            return False
+        else:
+            list_of_numbers = sorted([card.number for card in colour_cards])
+            for i in range(1, len(list_of_numbers)):
+                if list_of_numbers[i] != list_of_numbers[i-1] + 1:
+                    if joker_cards_count == 0:
+                        return False
+                    joker_cards_count -= 1
+            return True
+    
+    # Check if the set is either a valid Group or a valid Run
+    # None -> bool
+    def is_valid(self, cards):
+        return self.is_group(cards) or self.is_run(cards)              
+    
+    def is_first_move_valid(self, cards):
+        if self.is_valid(cards):                
+            total_points = sum(card.number for card in cards)
+            if total_points >= 30:
+                return True
+        else:
+            return False
+class Group(Set):
+    def __init__(self,cards):
+        super(),__init__(cards)
+        self.group = True
+        self.run = False
+
+class Run(Set):
+    def __init__(self,cards):
+        super(),__init__(cards)
+        self.group = False
+        self.run = True
