502035ac4e28a5695402afec4867c276c5d2ea93
Saharat Prangpitak <saharat.prangpitak@gmail.com>
2023-11-17 11:25:20 +0000

    Change filename: set.py -> cardset.py to avoid a problem

--- board.py
+++ board.py
@@ -1,5 +1,3 @@
-import card
-
 class Board:
     def __init__(self) -> None:
         self.board = []
@@ -9,20 +7,6 @@ class Board:
     
     def __repr__(self) -> str:
         return self.__str__()
-    
-    '''
-    Actually, I don't think player can add a single card in boards, 
-    even if it happened, it should be better if it is also a list with only one card object.
-    '''
-    # Add a card or cards to the board
-    # List[Card] or Card -> None
-    def add_cards(self, cards):
-        if isinstance(cards, list):
-            self.board.extend(cards)
-        elif isinstance(cards, card.Card):
-            self.board.append(cards)
-        else:
-            raise TypeError("Argument cards must be a list or a Card instance")
     
     def add_one_card_set(self, card_set):
         self.board.append(card_set)
