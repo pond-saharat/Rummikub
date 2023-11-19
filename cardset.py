684d6501ad4ac56487bca90c476736fafdaaa64c
Zach Gao <zihegao@outlook.com>
2023-11-19 02:50:38 +0000

    modify is_run() and is_group() function

--- cardset.py
+++ cardset.py
@@ -3,7 +3,7 @@ import card
 from config import *
 
 class CardSet:
-    def __init__(self,cards):
+    def __init__(self, cards):
         self.cards = cards
 
     # Create a new set of cards
@@ -20,13 +20,13 @@ class CardSet:
     # Check if the cards are valid for being classified as Group
     # List[Card] -> bool
     @staticmethod
-    def is_group(cards):
+    def is_group(self):
         # a valid group needs at least 3 cards and there are only 4 different colours
-        if len(cards) < 3 or len(cards) > NUM_OF_COLOURS:
+        if len(self.cards) < 3 or len(self.cards) > NUM_OF_COLOURS:
             return False
         
         # Get the colour cards (regular cards) from a pack of cards
-        colour_cards = card.Card.get_colour_cards(cards)   
+        colour_cards = card.Card.get_colour_cards(self.cards)   
         # a group is cards with the same number but different colours, 
         # Joker is wildcard, so only consider colour cards here
         same_num_in_hand = len(set([card.number for card in colour_cards]))
@@ -39,15 +39,15 @@ class CardSet:
     # Check if the cards are valid for being classified as Run
     # List[Card] -> bool
     @staticmethod
-    def is_run(cards):
+    def is_run(self):
         # a valid run needs at least 3 cards and there are only 13 different numbers each colour
-        if len(cards) < 3 or len(cards) > NUM_OF_CARDS_EACH_COLOUR:
+        if len(self.cards) < 3 or len(self.cards) > NUM_OF_CARDS_EACH_COLOUR:
             return False
         
         # Get the colour cards (regular cards) from a pack of cards
-        colour_cards = card.Card.get_colour_cards(cards)
+        colour_cards = card.Card.get_colour_cards(self.cards)
         # Get the count of Joker cards from a pack of cards
-        joker_cards_count = len(card.Card.get_joker_cards(cards)) #len([card for card in cards if card.number == 30]) 
+        joker_cards_count = len(card.Card.get_joker_cards(self.cards)) #len([card for card in cards if card.number == 30]) 
         # a run needs same colour for colour/regular cards
         if len(set([card.colour for card in colour_cards])) != 1:
             return False
@@ -64,12 +64,12 @@ class CardSet:
     
     # Check if the CardSet is either a valid Group or a valid Run
     # None -> bool
-    def is_valid(self, cards):
-        return self.is_group(cards) or self.is_run(cards)              
+    def is_valid(self):
+        return self.is_group() or self.is_run()              
     
-    def is_first_move_valid(self, cards):
-        if self.is_valid(cards):                
-            total_points = sum(card.number for card in cards)
+    def is_first_move_valid(self):
+        if self.is_valid():                
+            total_points = sum(card.number for card in self.cards)
             if total_points >= 30:
                 return True
         else:
